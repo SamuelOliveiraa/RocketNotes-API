cf46d48e7cd844cfbe49cf9af84d25141eb88947
Samuel Oliveira <oliveira_samuel14@hotmail.com>
2024-06-06 15:16:43 -0300

    Update UsersController.js

--- src/controllers/UsersController.js
+++ src/controllers/UsersController.js
@@ -16,6 +16,6 @@ class UsersController {
 
     res.status(201).json({ message: "Usuario criado com sucesso", error: false, data: { name, email } });
   }
-}
+} 
 
 module.exports = UsersController;
